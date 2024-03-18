788fd746e44c6d6618475ac431e5d13a20600ebb
Sanjay Baro <sanjaybaro2017@gmail.com>
2024-03-18 11:07:27 +0530

    adding Notes

--- usered/c12/src/App.jsx
+++ usered/c12/src/App.jsx
@@ -1,9 +1,39 @@
 // import './App.css'
+import { useReducer } from "react";
+
+
+/*NOTES----->
+ making reducer function to called aaction obj as an paramtere
+ dispatch(argument)---> argument == action {}
+ reducer (param1=initialstate, param2=action {})
+ action is JS Obj {}---> {type,payload} as a key here 
+ */
+
+const reducer = (state, action) => {
+  if (action.type === "INC") {
+    return state + 1; //what state should return because reducer is responsible for state change
+  }
+};
 
 function App() {
-  return <>
-  <h1>Learning Use Reducer</h1>
-  </>;
+  // const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(reducer, 0);
+
+  return (
+    <>
+      <h1>Learning Use Reducer</h1>
+      <h1>Count :{state}</h1>
+      <button
+        onClick={() => {
+          dispatch({ type: "INC" });
+        }}
+      >
+        INC
+      </button>
+      <button>DEC</button>
+      <button>RESET</button>
+    </>
+  );
 }
 
 export default App;
